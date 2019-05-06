fdf5826fac98ddfc435d7a62850fead812580a50
Jonathan Lopez <jonathanls95@gmail.com>
2019-05-06 20:08:46 +0200

    Code refactor and minor fixes

--- TelegramBot/Startup.cs
+++ TelegramBot/Startup.cs
@@ -13,141 +13,121 @@ namespace TelegramBot
     public class Startup
     {
         public static readonly TelegramBotClient Bot = new TelegramBotClient(ConfigurationManager.AppSettings["TelegramBotKey"]);
-        public static readonly List<string> Insultos = new List<string>()
-        {
-            "idiota", "gilipollas", "tonto", "capullo", "cabron", "imbecil", "lerdo"
-        };
-        public static Dictionary<string, string> FrasesAprendidas = new Dictionary<string, string>();
+        public static Dictionary<string, string> LearnedPhrases = new Dictionary<string, string>();
+        public static string LearnedPhrasesPath;
 
         static void Main(string[] args)
         {
+            LearnedPhrasesPath = System.IO.Directory.GetCurrentDirectory() + "\\LearnedPhrases.json";
+
             Bot.OnMessage += OnMessage;
-            FrasesAprendidas = JsonConvert.DeserializeObject<Dictionary<string, string>>(System.IO.File.ReadAllText(System.IO.Directory.GetCurrentDirectory() + "\\FrasesAprendidas.json"));
+
+            if (System.IO.File.Exists(LearnedPhrasesPath))
+            LearnedPhrases = JsonConvert.DeserializeObject<Dictionary<string, string>>(System.IO.File.ReadAllText(LearnedPhrasesPath));
 
             Bot.StartReceiving();
             Console.ReadLine();
-
-            System.IO.File.WriteAllText(System.IO.Directory.GetCurrentDirectory() + "\\FrasesAprendidas.json", JsonConvert.SerializeObject(FrasesAprendidas));
             Bot.StopReceiving();
         }
 
         private static async void OnMessage(object sender, MessageEventArgs e)
         {
-            if(e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Text && e.Message.Date.AddMinutes(1) < DateTime.Now)
+            if(e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Text && e.Message.Date.AddMinutes(1) > DateTime.UtcNow)
             {
                 string messageText = e.Message.Text.ToLower();
-                if (messageText.Contains("patatuela"))
-                {
-                    await Bot.SendTextMessageAsync(e.Message.Chat.Id, "Has dicho patatuela? Esa es mi amiga!");
-                }
-                if (messageText.ContainsAny(Insultos))
-                {
-                    await Bot.SendTextMessageAsync(e.Message.Chat.Id, "Como diría mufasa... eso tiene intencionalidad agresiva");
-                }
+                long chatId = e.Message.Chat.Id;
+
+                // LearnedPhrases
                 if (messageText.Contains("/remove"))
                 {
                     int position = e.Message.Text.IndexOf("/remove") + 8;
                     string keyToRemove = messageText.Substring(position);
-                    if (FrasesAprendidas.ContainsKey(keyToRemove))
+                    if (LearnedPhrases.ContainsKey(keyToRemove))
                     {
-                        FrasesAprendidas.Remove(keyToRemove);
-                        await Bot.SendTextMessageAsync(e.Message.Chat.Id, "Borrado!");
+                        LearnedPhrases.Remove(keyToRemove);
+                        await Bot.SendTextMessageAsync(chatId, "Borrado!");
                     }
                     else
                     {
-                        await Bot.SendTextMessageAsync(e.Message.Chat.Id, "No he encontrado esa frase para borrar");
+                        await Bot.SendTextMessageAsync(chatId, "No he encontrado esa frase para borrar");
                     }
                 }
-                if (messageText.ContainsAny(FrasesAprendidas.Keys))
+                else if (messageText.ContainsAny(LearnedPhrases.Keys))
                 {
-                    foreach (var key in messageText.GetAllContainingKeysIn(FrasesAprendidas.Keys.ToList()))
+                    foreach (var key in messageText.GetAllContainingKeysIn(LearnedPhrases.Keys.ToList()))
                     {
-                        await Bot.SendTextMessageAsync(e.Message.Chat.Id, FrasesAprendidas[key]);
+                        await Bot.SendTextMessageAsync(chatId, LearnedPhrases[key]);
                     }
                 }
-                if (messageText.ContainsAll(new List<string>() { "amigo", "aprende a contestar" }))
+                else if (messageText.ContainsAll(new List<string>() { "amigo", "aprende a contestar" }))
                 {
                     int position = e.Message.Text.IndexOf("aprende a contestar") + 20;
                     List<string> nuevaFrase = new List<string>();
+                    bool learned = false;
+
                     foreach (Match match in Regex.Matches(messageText, "\"([^\"]*)\""))
                         nuevaFrase.Add(match.ToString());
 
                     if (nuevaFrase.Count == 2)
                     {
                         string key = nuevaFrase.Last().ToLower().Replace("\"", string.Empty);
-                        string value = nuevaFrase.First().ToLower().Replace("\"", string.Empty);
-                        if (key.Length >= 3)
+                        string value = nuevaFrase.First().Replace("\"", string.Empty);
+
+                        if (!string.IsNullOrEmpty(key) && 
+                            !string.IsNullOrEmpty(value) && 
+                            key.Length >= 3)
                         {
-                            if (FrasesAprendidas.ContainsKey(key))
+                            if (LearnedPhrases.ContainsKey(key))
                             {
-                                FrasesAprendidas[key] = value;
+                                LearnedPhrases[key] = value;
                             }
                             else
                             {
-                                FrasesAprendidas.Add(key, value);
+                                LearnedPhrases.Add(key, value);
                             }
-                            await Bot.SendTextMessageAsync(e.Message.Chat.Id, "Aprendido!");
+                            await Bot.SendTextMessageAsync(chatId, "Aprendido!");
+                            System.IO.File.WriteAllText(LearnedPhrasesPath, JsonConvert.SerializeObject(LearnedPhrases));
+
+                            learned = true;
                         }
                     }
-                    else
+                    
+                    if (!learned)
                     {
-                        await Bot.SendTextMessageAsync(e.Message.Chat.Id, "Eso no lo he podido aprender...");
+                        await Bot.SendTextMessageAsync(chatId, "Eso no lo he podido aprender...");
                     }
                 }
                 else if (messageText.ContainsAll(new List<string>() { "amigo", "di" }))
                 {
                     int position = e.Message.Text.IndexOf("di") + 2;
-                    await Bot.SendTextMessageAsync(e.Message.Chat.Id, e.Message.Text.Substring(position));
+                    await Bot.SendTextMessageAsync(chatId, e.Message.Text.Substring(position));
                 }
-                
-                if (messageText.Contains("mierda de bot"))
+
+                // SPAM
+                if (messageText.Contains("/start") &&
+                    e.Message.Chat.Type == Telegram.Bot.Types.Enums.ChatType.Group)
                 {
-                    await Bot.SendTextMessageAsync(e.Message.Chat.Id, "Para mierda tú, cabesahuevo");
+                    await Bot.SendTextMessageAsync(chatId, "Quereis empezar una partida? Eso merece un buen patataspam!!");
+                    await SpamAdmins(chatId);
                 }
-                if (messageText.Contains("tu ano"))
+                else if (messageText.Contains("patataspa") &&
+                    e.Message.Chat.Type == Telegram.Bot.Types.Enums.ChatType.Group)
                 {
-                    await Bot.SendTextMessageAsync(e.Message.Chat.Id, "El culo tuyo");
-                }
-                if (messageText.Contains("cabesahuevo"))
-                {
-                    await Bot.SendTextMessageAsync(e.Message.Chat.Id, "Amigoooo amigooo amigoooooo amigoo");
-                }
-                if (messageText.Contains("patataspa"))
-                {
-                    if (e.Message.Chat.Type == Telegram.Bot.Types.Enums.ChatType.Group)
-                    {
-                        await SpamAdmins(e);
-                    }
-                }
-                if (messageText.Contains("/start"))
-                {
-                    if (e.Message.Chat.Type == Telegram.Bot.Types.Enums.ChatType.Group)
-                    {
-                        await Bot.SendTextMessageAsync(e.Message.Chat.Id, "Quereis empezar una partida? Eso merece un buen patataspam!!");
-                        await SpamAdmins(e);
-                    }
-                }
-                if (messageText.Contains("indepe"))
-                {
-                    await Bot.SendTextMessageAsync(e.Message.Chat.Id, "Visca la republica catalana! Visca puigdemont!");
-                }
-                if (messageText.Contains("/suicidio"))
-                {
-                    await Bot.SendTextMessageAsync(e.Message.Chat.Id, "ALLAHU AKBAAAAAR!");
+                    await SpamAdmins(chatId);
                 }
             }
         }
 
-        private static async System.Threading.Tasks.Task SpamAdmins(MessageEventArgs e)
+        private static async System.Threading.Tasks.Task SpamAdmins(long chatId)
         {
-            ChatMember[] members = await Bot.GetChatAdministratorsAsync(e.Message.Chat.Id);
+            ChatMember[] members = await Bot.GetChatAdministratorsAsync(chatId);
             string spamMsg = "";
             foreach (var member in members)
             {
                 if (!string.IsNullOrEmpty(member.User.Username))
                     spamMsg += $"@{member.User.Username} ";
             }
-            await Bot.SendTextMessageAsync(e.Message.Chat.Id, spamMsg);
+            await Bot.SendTextMessageAsync(chatId, spamMsg);
         }
     }
 }
